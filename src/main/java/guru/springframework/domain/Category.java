dca51f203eebd08a3667ebbef7fdefa6085a77ca
doxcom <aldoperez1319@gmail.com>
2024-09-10 15:36:35 -0600

    quitting lombok to fix issues

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,11 +1,9 @@
 package guru.springframework.domain;
 
-import lombok.*;
-
 import javax.persistence.*;
 import java.util.Set;
 
-@Data //Data is going include, getter, setter equals and hashcode on this class
+//Data is going include, getter, setter equals and hashcode on this class
 @Entity
 public class Category {
 
@@ -17,4 +15,30 @@ public class Category {
     @ManyToMany(mappedBy = "categories") //this will be on the join table
     private Set<Recipe> recipes;
 
+    public Category() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return this.recipes;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,12 +1,10 @@
 package guru.springframework.domain;
 
-import lombok.*;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
 //RECIPE is going to be Primary Object
-@Data
 @Entity
 public class Recipe {
 //Recipe and ingredient is a bi-directional
@@ -32,21 +30,23 @@ public class Recipe {
     private Set<Ingredient> ingredients = new HashSet<>(); //Set is an unordered collection of objects in which duplicate values cannot be stored
 
 
-
     @Lob //to store image or big objects into database
-    private Byte [] image;
+    private Byte[] image;
 
     @Enumerated(EnumType.STRING)//we use type string will overwrite behaviour of the enum
-    private  Difficulty difficulty; //will going to store the enum value into database
+    private Difficulty difficulty; //will going to store the enum value into database
 
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes; //one to one property
 
     @ManyToMany
-    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name ="recipe_id"),
-            inverseJoinColumns = @JoinColumn( name = "category_id"))
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    public Recipe() {
+    }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
@@ -58,6 +58,106 @@ public class Recipe {
         ingredient.setRecipe(this);
         this.ingredients.add(ingredient);
         return this;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Integer getPrepTime() {
+        return this.prepTime;
+    }
+
+    public Integer getCookTime() {
+        return this.cookTime;
+    }
+
+    public Integer getServings() {
+        return this.servings;
+    }
+
+    public String getSource() {
+        return this.source;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getDirections() {
+        return this.directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return this.ingredients;
+    }
+
+    public Byte[] getImage() {
+        return this.image;
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public Notes getNotes() {
+        return this.notes;
+    }
+
+    public Set<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
 }
