951ef75f06436bf7258bd1fc9fc128f161930fd0
doxcom <aldoperez1319@gmail.com>
2024-10-19 04:25:23 -0600

    Refactoring Recipe Controller

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,7 +1,6 @@
 package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
-import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
